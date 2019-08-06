168107008a7e489be53ecd8d97eab321b0daafe0
bcho718 <49840760+bcho718@users.noreply.github.com>
2019-08-06 15:04:53 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,31 +2,41 @@ palin_string = input('Enter a string: ')
 
 
 def palindrome(palin_string):
+
+  #find the length of the input
   ranger = len(palin_string)
   start = 0
   end = 0
+
+  #add all of the palindrome that is found in the input string
   palindrome_list = []
+  palindrome_final = []
   final_word = 0
   for i in range(1, int(ranger)):
     start = i - 1
     end = i + 1 
+
+    #check to see if there are words that start and end with same letter
     while start >= 0 and end < ranger:
       if palin_string[start] == palin_string[end]:
         palin1 = palin_string[start:end+1]
         palindrome_list.append(palin1)
-        start = start - 1
-        end = end + 1
+        start -= 1
+        end += 1
         continue
       else:
-        start = start - 1
-        end = end + 1
+        start -+ 1
+        end += 1
         continue
-  # print(palindrome_list)
+  #list of strings that start / end with same letter      
+  print(palindrome_list)
+
+  #check to see if the strings in palindrome_list are palindrome 
   for i in palindrome_list:
     if i == i[::-1]:
-      if len(i) > len(str(final_word)):
-        final_word = i
-  print(final_word)
-
+      palindrome_final.append(i)
+      palindrome_final.sort()
+  #print the longest string    
+  print(palindrome_final[-1])
 
 palindrome(palin_string)
